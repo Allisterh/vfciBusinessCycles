5ee4888890737a75d3d4907e57755c8d2306da71
matdehaven <matthew_dehaven@brown.edu>
2023-07-19 13:14:36 -0400

    Added replication test

---
+++ tests/test_bca-replication.R
@@ -0,0 +1,19 @@
+test_that("Replicated Classical VAR IRFs", {
+  require(data.table)
+  require(dplyr)
+
+  df <- 
+    fread(here("./data/replicated_bca_classical_VAR_IRF.csv")) |>
+    mutate(
+        value = -1 * value,
+        median = -1 * median
+    ) |>
+    mutate(
+        variable = factor(variable, levels = c("unemployment", "output", "hours_worked", "investment", "consumption", "TFP", "productivity", "labor_share", "inflation", "interest"), ordered = T)
+    ) |>
+    dplyr::select(h, variable, value, model) |>
+    tidyr::pivot_wider(names_from = model, values_from = value)
+
+  expect_equal(df$Replication, df$classical_fd, tolerance = 0.05)
+
+})
