c0f9a39688513058ba24bd64f3e794806d4a1f21
matdehaven <matthew_dehaven@brown.edu>
2023-08-12 18:39:59 -0400

    Add bca_mn_priors

---
+++ code/helpers/bca_mn_priors.R
@@ -0,0 +1,63 @@
+##
+##  `bvartools` calculates priors slightly differently than
+##  the BCA paper.  This recreates the priors from BCA.
+##
+##
+bca_mn_priors <- function(
+    data,
+    lags,
+    gamma1,
+    gamma2,
+    gamma3,
+    gamma4
+) {
+
+k <- ncol(data)
+N <- nrow(data)
+m <- (k * k * lags) + k
+
+## Set mean values for the first lags to 1, 0 for all else
+mu <- matrix(0, m, 1)
+mu[seq(1, k * k, by = k + 1)] <- 1
+
+
+## Construct SE by running AR(1) for each variable
+se <- rep(0, k)
+for (i in 1:k){
+    yi <- t(data[-1, i])
+    xi <- rbind(t(data[-N, i]), rep(1, N - 1))
+    betas <- tcrossprod(yi, xi) %*% solve(tcrossprod(xi))
+    yi_hat <- betas %*% xi
+    epsilon <- yi - yi_hat
+    df <- ncol(yi) - nrow(xi)
+    se[i] <- sqrt(tcrossprod(epsilon, epsilon) / df)
+}
+
+## Calculate the variances for coefficients using BCA formula
+v <- matrix(0, m, m)
+for (i in 1:k) {
+    for (j in 1:k) {
+        for (l in 1:lags){
+            index <- i + (j - 1) * k + (l - 1) * k * k
+            if (i == j) {
+                v[index, index] <- (gamma1 / l^gamma3)^2
+            } else {
+                se2 <- (se[i] / se[j])^2
+                v[index, index] <- se2 * (gamma1 * gamma2 / l^gamma3)^2
+            }
+        }
+    }
+    index <- (k * k * lags) + i
+    v[index, index] <- (se[i] * gamma4)^2
+}
+
+v_i <- solve(v, tol = 1e-20)
+
+priors <- list(
+    mu = mu,
+    v_i = v_i,
+    sigma_i = diag(se)
+)
+
+return(priors)
+}
