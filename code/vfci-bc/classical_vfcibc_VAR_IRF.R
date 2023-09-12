71b14fa0287e94732c06849271112c3585e06cc1
matdehaven <matthew_dehaven@brown.edu>
2023-09-12 13:53:11 -0400

    Exploring vfci irfs

--- code/vfci-bc/classical_vfcibc_VAR_IRF.R
+++ code/vfci-bc/classical_vfcibc_VAR_IRF.R
@@ -9,7 +9,7 @@ require(fevdid)
 ## Pull the correct vintage of the BCA data
 vfciBCdata <- fread("./data/vfciBC_data.csv") |>
     filter(date <= as.Date("2017-01-01"))
-vfciBCdata[, -"date"]
+vfciBCdata
 
 ## Target the BC frequency and umemployment variable
 bc_freqs <- c(2 * pi / 32, 2 * pi / 6)
@@ -21,16 +21,99 @@ v <- VAR(vfciBCdata[, -"date"], p = 2, type = "const")
 
 ## Identify the shock
 mv0 <- id_fevdfd(v0, target = "unemployment", freqs = bc_freqs)
+
+grid <- rbindlist(list(
+    list("unemployment", bc_freqs[1], bc_freqs[2], "pos"),
+    list("unemployment", 2 * pi / 100, 2 * pi / 40, "pos"),
+    list("unemployment", 2 * pi / 40, 2 * pi / 20, "pos"),
+    list("unemployment", 2 * pi / 20, 2 * pi / 10, "pos"),
+    list("unemployment", 2 * pi / 10, 2 * pi / 5, "pos"),
+    list("unemployment", 2 * pi / 5, 2 * pi / 2, "pos"),
+    list("vfci", bc_freqs[1], bc_freqs[2], "pos"),
+    list("vfci", 2 * pi / 100, 2 * pi / 40, "pos"),
+    list("vfci", 2 * pi / 40, 2 * pi / 20, "pos"),
+    list("vfci", 2 * pi / 20, 2 * pi / 10, "pos"),
+    list("vfci", 2 * pi / 10, 2 * pi / 5, "pos"),
+    list("vfci", 2 * pi / 5, 2 * pi / 2, "pos")
+    ))
+names(grid) <- c("target", "freq_l", "freq_h", "sign")
+grid
+
+irf_df <-
+rbindlist(lapply(seq_len(nrow(grid)), function(i) {
+    mv <- id_fevdfd(
+        v,
+        target = grid[[i, "target"]],
+        freqs = c(grid[[i, "freq_l"]], grid[[i, "freq_h"]]),
+        sign = grid[[i, "sign"]]
+        )
+
+    irf_df <- vars::irf(mv, impulse = "Main", n.ahead = 40) |> 
+        setDT() |>
+        mutate(
+            target = grid[[i, "target"]],
+            freq_l = grid[[i, "freq_l"]],
+            freq_h = grid[[i, "freq_h"]],
+            sign = grid[[i, "sign"]]
+        )
+}))
+
+irf_df |>
+    filter(target == "vfci") |>
+    mutate(label = paste(target, ":", round(freq_l, 2), "-", round(freq_h, 2))) |>
+    ggplot(aes(
+        x = h,
+        y = irf,
+        color = label
+    )) +
+    geom_hline(yintercept = 0) +
+    geom_line() +
+    facet_wrap(vars(response), scales = "free_y")
+
+irf_df |>
+    filter(target == "unemployment") |>
+    mutate(label = paste(target, ":", round(freq_l, 2), "-", round(freq_h, 2))) |>
+    ggplot(aes(
+        x = h,
+        y = irf,
+        color = label
+    )) +
+    geom_hline(yintercept = 0) +
+    geom_line() +
+    facet_wrap(vars(response), scales = "free_y")
+
+## Two Similar ones
+irf_df |>
+    filter(freq_l == 2 * pi / 40) |>
+    mutate(label = paste(target, ":", round(freq_l, 2), "-", round(freq_h, 2))) |>
+    ggplot(aes(
+        x = h,
+        y = irf,
+        color = label
+    )) +
+    geom_hline(yintercept = 0) +
+    geom_line() +
+    facet_wrap(vars(response), scales = "free_y")
+
+
+
+
+
+#####
+#####
+#####
+
+
 mv <- id_fevdfd(v, target = "unemployment", freqs = bc_freqs)
 mv_vfci_bc <- id_fevdfd(v, target = "vfci", freqs = bc_freqs, sign = "neg")
-mv_vfci_sr <- id_fevdfd(v, target = "vfci", freqs = c(0.175, 0.3), sign = "neg")
+mv_vfci_sr <- id_fevdfd(v, target = "vfci", freqs = c(2 * pi / 40, 2 * pi / 20), sign = "neg")
 
 
 ## Get the IRF
-mv0_irf <- vars::irf(mv0, impulse = "Main", n.ahead = 40)
-mv_irf <- vars::irf(mv, impulse = "Main", n.ahead = 40)
-mv_vfci_bc_irf <- vars::irf(mv_vfci_bc, impulse = "Main", n.ahead = 40)
-mv_vfci_sr_irf <- vars::irf(mv_vfci_sr, impulse = "Main", n.ahead = 40)
+mv0_irf <- vars::irf(mv0, impulse = "Main", n.ahead = 40) |> setDT()
+mv_irf <- vars::irf(mv, impulse = "Main", n.ahead = 40) |> setDT()
+mv_vfci_bc_irf <- vars::irf(mv_vfci_bc, impulse = "Main", n.ahead = 40) |> setDT()
+mv_vfci_sr_irf <- vars::irf(mv_vfci_sr, impulse = "Main", n.ahead = 40) |> setDT()
 
 mv_fevdfd <-  mv |> fevdfd()
 mv_fevdfd$vfci |>
@@ -46,53 +129,27 @@ bca_irf_df <- bca_irf_df |>
     filter(model == "classical_fd") |>
     mutate(version = "original") |>
     rename(h = "horizon") |>
-    mutate(h = h) |>
-    rename(lower = "pctl_16") |>
-    rename(upper = "pctl_84") |>
-    rename(value = "varirf") |>
+    rename(response = "variable") |>
+    rename(irf = "varirf") |>
     mutate(shock = "Main") |>
     mutate(target = "u_bc")
 
-mv0_irf_df <- mv0_irf[[1]] |>
-    as_tibble() |>
-    rename(h = "V1") |>
-    tidyr::pivot_longer(-h) |>
-    mutate(variable = stringr::str_extract(name, "(?<=%->%).*$")) |>
-    mutate(shock = stringr::str_extract(name, "(?<= ).*(?= )")) |>
-    dplyr::select(!name) |>
+mv0_irf_df <- mv0_irf |>
     mutate(model = "classical_fd") |>
     mutate(version = "bca_variables") |>
     mutate(target = "u_bc")
 
-mv_irf_df <- mv_irf[[1]] |>
-    as_tibble() |>
-    rename(h = "V1") |>
-    tidyr::pivot_longer(-h) |>
-    mutate(variable = stringr::str_extract(name, "(?<=%->%).*$")) |>
-    mutate(shock = stringr::str_extract(name, "(?<= ).*(?= )")) |>
-    dplyr::select(!name) |>
+mv_irf_df <- mv_irf |>
     mutate(model = "classical_fd") |>
     mutate(version = "vfci") |>
     mutate(target = "u_bc")
 
-mv_vfci_bc_irf_df <- mv_vfci_bc_irf[[1]] |>
-    as_tibble() |>
-    rename(h = "V1") |>
-    tidyr::pivot_longer(-h) |>
-    mutate(variable = stringr::str_extract(name, "(?<=%->%).*$")) |>
-    mutate(shock = stringr::str_extract(name, "(?<= ).*(?= )")) |>
-    dplyr::select(!name) |>
+mv_vfci_bc_irf_df <- mv_vfci_bc_irf |>
     mutate(model = "classical_fd") |>
     mutate(version = "vfci") |>
     mutate(target = "vfci_bc")
 
-mv_vfci_sr_irf_df <- mv_vfci_sr_irf[[1]] |>
-    as_tibble() |>
-    rename(h = "V1") |>
-    tidyr::pivot_longer(-h) |>
-    mutate(variable = stringr::str_extract(name, "(?<=%->%).*$")) |>
-    mutate(shock = stringr::str_extract(name, "(?<= ).*(?= )")) |>
-    dplyr::select(!name) |>
+mv_vfci_sr_irf_df <- mv_vfci_sr_irf |>
     mutate(model = "classical_fd") |>
     mutate(version = "vfci") |>
     mutate(target = "vfci_lr")
