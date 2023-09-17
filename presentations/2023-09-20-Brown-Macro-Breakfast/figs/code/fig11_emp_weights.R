bbd2492e97403e41e96fe9f4bbf76193e05678d1
matdehaven <matthew_dehaven@brown.edu>
2023-09-17 16:45:51 -0400

    Adding empirical shock weights

--- presentations/2023-09-20-Brown-Macro-Breakfast/figs/code/fig11_emp_weights.R
+++ presentations/2023-09-20-Brown-Macro-Breakfast/figs/code/fig11_emp_weights.R
@@ -3,8 +3,8 @@ require(ggplot2)
 require(dplyr)
 source("./presentations/2023-09-20-Brown-Macro-Breakfast/figs/code/theme_pres.R")
 
-resid_data <- fread("./data/residual-mbc-shock/weights.csv")
-data <- fread("./data/classical_vfcibc_weights.csv")
+resid_data <- fread("./data/residual-mbc-shock/B.csv")
+data <- fread("./data/classical_vfcibc_B.csv")
 
 data <- rbindlist(list(
     data[target == "unemployment" & sign == "pos" & period_l == 6 & period_h == 32, .(variable, weight, model = "u0632")],
@@ -17,7 +17,7 @@ data <- rbindlist(list(
 data[, model := factor(model, levels = c("u0632", "v2232", "resid", "u2232", "v0632"), ordered = TRUE)]
 data[, variable := factor(variable, levels = arrange(data[model == "u0632"], weight)$variable, ordered = TRUE)]
 
-plot <-
+plot <- 
     data|>
     ggplot(aes(
         x = variable,
@@ -33,7 +33,7 @@ plot <-
         x = NULL,
         y = "Rotation Weight"
     ) +
-    scale_x_discrete(guide = guide_axis(n.dodge = 2)) + 
+    scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
     scale_fill_manual(
         values = c(
             u0632 = "goldenrod",
@@ -57,7 +57,7 @@ plot <-
 
 
 ggsave(
-    "./presentations/2023-09-20-Brown-Macro-Breakfast/figs/fig7_weights.pdf",
+    "./presentations/2023-09-20-Brown-Macro-Breakfast/figs/fig11_emp_weights.pdf",
     plot,
     units = "in",
     width = 4.5,
--- presentations/2023-09-20-Brown-Macro-Breakfast/figs/code/fig8_hist_shocks.R
+++ presentations/2023-09-20-Brown-Macro-Breakfast/figs/code/fig8_hist_shocks.R
@@ -42,7 +42,7 @@ plot <-
     theme_pres +
     theme(legend.position = c(0.75, 0.3))
 
-
+plot
 ggsave(
     "./presentations/2023-09-20-Brown-Macro-Breakfast/figs/fig8_hist_shocks.pdf",
     plot,
@@ -50,3 +50,59 @@ ggsave(
     width = 4.5,
     height = 2
     )
+
+#####
+data |>
+    tidyr::pivot_wider(names_from = "model", values_from = "shock") |>
+    ggplot(aes(
+        x = u0632,
+        y = v2232
+    )) +
+    geom_vline(xintercept = 0) + 
+    geom_hline(yintercept = 0) +
+    geom_smooth(se = FALSE, method = "lm") +
+    geom_point() +
+    coord_fixed()
+
+lm_data <- 
+data |>
+    tidyr::pivot_wider(names_from = "model", values_from = "shock") 
+lm_data |>
+    lm(formula = "u0632 ~ v2232") |>
+    summary()
+
+stats::cor(lm_data$u0632, lm_data$v2232)
+
+####
+plot_data <- copy(data)
+plot_data[, shock_ma := frollmean(shock, n = 20, align = "center"), by = .(model)]
+plot_data[, shock_ma_norm := scale(shock_ma), by = .(model)]
+plot <-
+    plot_data[model != "resid"] |>
+    ggplot(aes(
+        x = date,
+        y = shock_ma_norm,
+        color = model
+    )) +
+    geom_hline(yintercept = 0) +
+    geom_line() +
+    labs(
+        x = NULL,
+        y = "Historical Shock"
+    ) +
+    scale_x_date(date_breaks = "5 years", date_labels = "%Y") +
+    scale_color_manual(
+        values = c(
+            u0632 = "goldenrod",
+            v2232 = "steelblue",
+            resid = "mediumorchid"
+        ),
+        labels = c(
+            u0632 = "MBC",
+            v2232 = "VFCI 22:32",
+            resid = "Residual MBC"
+        )
+    ) +
+    theme_pres +
+    theme(legend.position = c(0.75, 0.3))
+plot
