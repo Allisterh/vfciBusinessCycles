e430656aa23e576c5c85d552f98a0f546f171ede
matdehaven <matthew_dehaven@brown.edu>
2023-10-09 10:38:19 -0400

    Updated replication files

--- code/bca-replication/classical-var-irf/replicate_bca_classical_VAR_IRF.R
+++ code/bca-replication/classical-var-irf/replicate_bca_classical_VAR_IRF.R
@@ -12,7 +12,10 @@ bcadata <- fread("./data/bca_replication_data.csv") |>
 x <- bcadata[, -"date"]
 
 ## Load the original BCA IRFs
-bca_irf_df <- fread("./data/bca_original_var_results.csv")
+bca_irf_df <- fread("./data/bca_original_var_results.csv")[
+    model == "classical_fd"][,
+    version := "Original BCA"
+    ]
 
 ## Target the BC frequency and umemployment variable
 bc_freqs <- c(2 * pi / 32, 2 * pi / 6)
@@ -25,28 +28,20 @@ v <- VAR(x, p = 2, type = "const")
 mv <- id_fevdfd(v, target = target_var, freqs = bc_freqs)
 
 ## Get the IRF
-mv_irf <- irf(mv, impulse = "Main", n.ahead = 40)
-
-## Some data wrangling to get a clean dataframe
-## with original and replicated IRFs
-bca_irf_df <- bca_irf_df |>
-    filter(model == "classical_fd") |>
-    rename(h = "horizon") |>
-    mutate(h = h - 1) |>
-    rename(value = "varirf") |>
-    mutate(value = value) |>
-    mutate(shock = "Main")
-
-mv_irf_df <- mv_irf[[1]] |>
+mv_irf_df <- irf(mv, impulse = "Main", n.ahead = 40)$irf |>
     as_tibble() |>
-    rename(h = "V1") |>
-    tidyr::pivot_longer(-h) |>
-    mutate(variable = stringr::str_extract(name, "(?<=%->%).*$")) |>
-    mutate(shock = stringr::str_extract(name, "(?<= ).*(?= )")) |>
-    dplyr::select(!name) |>
-    mutate(model = "Replication")
+    mutate(
+        model = "classical_fd",
+        version = "Replication"
+        )
 
-comb_df <- rbindlist(list(mv_irf_df, bca_irf_df), use.names = TRUE, fill = TRUE)
+## Combine the two datasets, drop other models
+comb_df <- rbindlist(list(
+    mv_irf_df,
+    bca_irf_df
+    ), use.names = TRUE, fill = TRUE)
+
+comb_df <- comb_df[, .(h, impulse, response, irf, model, version)]
 
 ## Save the combined DF out to disk
 fwrite(comb_df, "./data/replicated_bca_classical_VAR_IRF.csv")
