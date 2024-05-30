de7b2503c41fbb00027dad16aff7d4830576067d
matdehaven <matthew_dehaven@brown.edu>
2024-05-30 11:45:22 -0400

    add FEV calculations too bootstrap

--- code/bca-replication/classical-var-irf/replicate_bca_classical_VAR_IRF_boot.R
+++ code/bca-replication/classical-var-irf/replicate_bca_classical_VAR_IRF_boot.R
@@ -25,16 +25,56 @@ mv <- id_fevdfd(v, target = target_var, freqs = bc_freqs)
 mv_irf <- vars::irf(mv, impulse = "Main", n.ahead = 40)$irf
 
 ## Bootstraps
-boot_df_resample_ba <-
+boots <-
   bootstrap(
     mv, id_fevdfd, nboot = 1000, n_ahead = 40,
     method = "resample", design = "recursive", bias_adjust = TRUE,
     target = "unemployment", freqs = bc_freqs
-  )$IRF_df |>
+  )
+
+boot_df_resample_ba <-
+  boots$IRF_df |>
   as.data.table() |>
   _[impulse == "Main"] |>
   _[, model := "classical_fd"] |>
   _[, version := "Replication"]
+
+## Calculate the FEVD
+fevdfd_df <-
+  purrr::map(purrr::set_names(1:1000, 1:1000), function(i) {
+    temp <- boots$VAR
+    temp$B <- boots$B_boots[, , i]
+    fevfd_df <- fevfd(temp)$fevfd
+    fevfd_df
+  }) |>
+  purrr::list_rbind(names_to = "i") |>
+  setDT()
+
+fevdfd_df[, total := sum(fevfd), by = .(i, f, response)]
+fevdfd_df[, fevdfd := fevfd / total]
+
+boot_summ <-
+  fevd_df |>
+  _[impulse == "Main"] |>
+  _[f %between% bc_freqs] |>
+  _[, .(
+    avg_fevdfd = mean(fevdfd),
+    med_fevdfd = median(fevdfd),
+    wavg_fevdfd = weighted.mean(fevdfd, total)
+  ), by = .(
+    i,
+    impulse,
+    response
+  )]
+
+boot_summ[, .(
+  lower_wavg = quantile(wavg_fevdfd, 0.16),
+  med_wavg = median(wavg_fevdfd),
+  upper_wavg = quantile(wavg_fevdfd, 0.84)
+), by = .(
+  impulse,
+  response
+)]
 
 ## Some data wrangling to get a clean dataframe
 ## with original and replicated IRFs
