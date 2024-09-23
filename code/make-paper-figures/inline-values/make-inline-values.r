c5ea69a5cc64369f9f7b0a16e15b34fc60e7868b
matdehaven <matthew_dehaven@brown.edu>
2024-09-23 10:56:56 -0400

    Rm previous paper figures

--- code/make-paper-figures/inline-values/make-inline-values.r
+++ code/make-paper-figures/inline-values/make-inline-values.r
@@ -12,30 +12,17 @@ library(data.table)
 library(vfciBCHelpers)
 
 data <- get_var_data()
-corrs <- fread("./data/paper-figures/tables/hs-cross-correlations.csv")
-avg_bc_fevdfds <- fread("./data/paper-figures/tables/avg-fevdfd-bc-freqs-max-share.csv")
 
 v <- list(
-  vfciUnemploymentMaxShareCorrelation =
-    round(corrs[var_x == "vfci" & var_y == "unemployment", corr], 2),
-  vfciOutputMaxShareCorrelation =
-    round(corrs[var_x == "vfci" & var_y == "output", corr], 2),
-  vfciLaborShareMaxShareCorrelation =
-    round(corrs[var_x == "vfci" & var_y == "labor_share", corr], 2),
   dataStartDate = zoo::as.yearqtr(min(data$date)),
-  dataEndDate = zoo::as.yearqtr(max(data$date)),
-  vfciFEVDFD = 100 * round(avg_bc_fevdfds[target == "vfci" & response == "vfci", avg_fevdfd], 2)
+  dataEndDate = zoo::as.yearqtr(max(data$date))
 )
 
 
 ## Define the Latex commands for each value
 values <- c(
-  def_latex_value("vfciUnemploymentMaxShareCorrelation", v$vfciUnemploymentMaxShareCorrelation),
-  def_latex_value("vfciOutputMaxShareCorrelation", v$vfciOutputMaxShareCorrelation),
-  def_latex_value("vfciLaborShareMaxShareCorrelation", v$vfciLaborShareMaxShareCorrelation),
   def_latex_value("dataStartDate", v$dataStartDate, math_mode = FALSE),
-  def_latex_value("dataEndDate", v$dataEndDate, math_mode = FALSE),
-  def_latex_value("vfciFEVDFD", v$vfciFEVDFD)
+  def_latex_value("dataEndDate", v$dataEndDate, math_mode = FALSE)
 )
 
 ## Write to disk
